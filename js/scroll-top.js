73d80210efcb41168cf552573eef481d486d2c1d
Valery Menelas <valerymenelas@gmail.com>
2016-11-11 08:11:42 -0500

    • Updated app.css to fix form-horizontal issue as well as actionBtn • Continue working on changes for claim-details.html • Moved tab-content from app.css to company-info.css • Added comments to scroll-top.js

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,24 +1,23 @@
 (function($) {
     $.fn.checkBackToTop = function() {
-        $('.pull-right .nav-pills li a').click(function() {
-
-            if ($(window).outerWidth() >= 767) {
-                if ($(window).scrollTop() > 82) {
+        $('.pull-right .nav-pills li a').click(function() { //call function base on click event
+            if ($(window).outerWidth() >= 767) { //check for desktop
+                if ($(window).scrollTop() > 82) { //when the current scroll bar height is greater than 82, 82 is the height that the claim-details div is hidden when scroll
                     $('html, body').animate({
                         scrollTop: 0
-                    }, 500);
-                    return true;
+                    }, 500); //scroll to top in 500ms
+                    return true; //callback
                 }
-            } else {
+            } else { //for mobile device
                 $('html, body').animate({
                     scrollTop: $(".claim-details").offset().top - 60
-                }, 1000);
-                return true;
+                }, 1000); //get position of .clain-detail  from top, 60 is a height to visible the content of .claim-detail from top.
+                return true; //callback
             }
         });
     }
-    $.fn.checkBackToTop();
+    $.fn.checkBackToTop(); //make checkBackToTop ready when document is ready  
     $(window).on('resize', function() {
-        $.fn.checkBackToTop();
+        $.fn.checkBackToTop(); //make checkBackToTop ready when window is resized
     });
 })(jQuery);
